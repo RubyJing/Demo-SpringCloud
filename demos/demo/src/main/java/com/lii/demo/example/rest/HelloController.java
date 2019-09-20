6dcb0b182950b38e0eb2deb62b11d85a9c7c8473
RubyJing <976235684@qq.com>
2019-09-20 11:46:22 +0800

    在demo中增加使用springCloudConfig的例子

--- demos/demo/src/main/java/com/lii/demo/example/rest/HelloController.java
+++ demos/demo/src/main/java/com/lii/demo/example/rest/HelloController.java
@@ -3,12 +3,27 @@ package com.lii.demo.example.rest;
 
 import com.springboot.cloud.common.core.entity.vo.Result;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
 
+@RefreshScope
 @RestController
 public class HelloController {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${username}")
+    private String username;
+
+//    @Value("${sdf}")
+//    private String sdf;
+
     @RequestMapping(method = RequestMethod.GET, value = "/hello/{name}")
     public String hello(@PathVariable String name) {
         return randomNumeric(2) + name;
@@ -19,4 +34,8 @@ public class HelloController {
         return Result.success(name + "success");
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/username")
+    public String userName(){
+        return env.getProperty("sdf","undifend");
+    }
 }
