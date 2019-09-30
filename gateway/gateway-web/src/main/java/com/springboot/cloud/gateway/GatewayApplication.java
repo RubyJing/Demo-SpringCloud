f12deed8896f90682db622ed38a38f44bc5bdb84
RubyJing <976235684@qq.com>
2019-09-30 15:57:54 +0800

    网关服务，用于路由转发，限流等

---
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients(basePackages = "com.springboot.cloud.auth.client")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringCloudApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
